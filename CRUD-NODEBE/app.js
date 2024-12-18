21950001166af5fd72a66992c2c0968b2a3f97fa
Natnael Berhanu <natman093@gmail.com>
2024-12-18 13:21:17 +0300

    Second Commit

--- CRUD-NODEBE/app.js
+++ CRUD-NODEBE/app.js
@@ -15,17 +15,20 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 
-const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-};
+// const corsOptions = {
+//   origin: 'http://localhost:5173',
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get("/",   (req, res) => {
   res.json("Back End");
 })
+
+app.get('/favicon.ico', (req, res) => res.status(204).end()); // Handle favicon requests
+
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
@@ -33,9 +36,14 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use("/api/vehicle", vehicleRoutes);  //base route is /api/vehicle in vehicleRoutes it will have a child route to to have the controller function 
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
+}
+
+module.exports = app;
 
 
 
