a96f45b81246c8ebbf7c1b0f5dca4372f2537512
Natnael Berhanu <natman093@gmail.com>
2024-12-18 15:41:43 +0300

    Fifth Commit

--- CRUD-NODEBE/app.js
+++ CRUD-NODEBE/app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 
 const corsOptions = {
-  origin: process.env.FRONT_END_URL || 'http://localhost:5173',
+  origin: 'https://vehicle-mini-frontend.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
