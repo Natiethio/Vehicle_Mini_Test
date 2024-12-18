c1cbf79fe51e834b394fab44521c119d628f2fc4
Natnael Berhanu <natman093@gmail.com>
2024-12-18 12:50:59 +0300

    Third Commit

--- CRUD-NODEBE/app.js
+++ CRUD-NODEBE/app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
