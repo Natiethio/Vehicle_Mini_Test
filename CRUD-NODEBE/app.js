c4dbbbf135a858e79987f9b2514a6c36a782a5c3
Natnael Berhanu <natman093@gmail.com>
2024-12-19 15:47:54 +0300

    Ninth commit

--- CRUD-NODEBE/app.js
+++ CRUD-NODEBE/app.js
@@ -24,7 +24,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.get("/",   (req, res) => {
-  res.json("Back End Upd"); 
+  res.json("Node BackEnd"); 
 })
 
 app.get('/favicon.ico', (req, res) => res.status(204).end()); // Handle favicon requests
